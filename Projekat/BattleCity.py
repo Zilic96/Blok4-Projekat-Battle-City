dd30f40cac145f1d52c6061ad92f8e899b0771a9
Nemanaj Zilic <Zilic96@users.noreply.github.com>
2018-12-28 17:54:52 +0100

    Moving/fireing in all directions

--- Projekat/BattleCity.py
+++ Projekat/BattleCity.py
@@ -46,11 +46,41 @@ from PyQt5.QtGui import *
 SCREEN_WIDTH            = 1200
 SCREEN_HEIGHT           = 800
 PLAYER_SPEED            = 3   # pix/frame
-PLAYER_BULLET_X_OFFSETS = [25,45]
+PLAYER_BULLET_X_OFFSETS = 23
 PLAYER_BULLET_Y         = 15
 BULLET_SPEED            = 10  # pix/frame
 BULLET_FRAMES           = 90
 FRAME_TIME_MS           = 16  # ms/frame
+playerPositions = [(0,0),(0,0)]
+
+maps = [
+(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
+(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
+(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
+(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
+(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
+(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
+(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
+(0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
+(0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
+(0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
+(0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
+(0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
+(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
+(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
+(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
+(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
+(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
+(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
+(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
+(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
+(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
+(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
+(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
+(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
+(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
+
+]
 
 class Player(QGraphicsPixmapItem):
     def __init__(self, image, parent = None):
@@ -63,34 +93,66 @@ class Player(QGraphicsPixmapItem):
         dx = 0
         dy = 0
         if Qt.Key_Left in keys_pressed:
-            dx -= PLAYER_SPEED
+            """print("PLAYER 2 :")
+            print(playerPositions[1][0])
+            print("PLAYER 1 :")
+            print(self.x()-25)"""
+            if  self.x() <= 0 :
+                dx -= 0
+            else:
+                dx -= PLAYER_SPEED
             self.setimage("Image/tankLeft")
         elif Qt.Key_Right in keys_pressed:
-            dx += PLAYER_SPEED
+            if self.x() >= 960:
+                dx -= 0
+            else:
+                dx += PLAYER_SPEED
             self.setimage("Image/tankRight")
         elif Qt.Key_Up in keys_pressed:
-            dy -= PLAYER_SPEED
+            if self.y() <= 0:
+                dy -= 0
+            else:
+                dy -= PLAYER_SPEED
             self.setimage("Image/tankTop")
         elif Qt.Key_Down in keys_pressed:
+            if self.y() <= 746:
+                dy -= 0
+            else:
+                dy -= PLAYER_SPEED
             dy += PLAYER_SPEED
             self.setimage("Image/tankBottom")
+        playerPositions[0] = (self.x()+dx, self.y()+dy)
         self.setPos(self.x()+dx, self.y()+dy)
 
     def game_update2(self, keys_pressed):
         dx = 0
         dy = 0
         if Qt.Key_A in keys_pressed:
-            dx -= PLAYER_SPEED
+            if self.x() <= 0 :
+                dx -= 0
+            else:
+                dx -= PLAYER_SPEED
             self.setimage2("Image/tank2Left")
         elif Qt.Key_D in keys_pressed:
-            dx += PLAYER_SPEED
+            if self.x() >= 960:
+                dx -= 0
+            else:
+                dx += PLAYER_SPEED
             self.setimage2("Image/tank2Right")
         elif Qt.Key_W in keys_pressed:
-            dy -= PLAYER_SPEED
+            if self.y() <= 0:
+                dy -= 0
+            else:
+                dy -= PLAYER_SPEED
             self.setimage2("Image/tank2Top")
         elif Qt.Key_S in keys_pressed:
-            dy += PLAYER_SPEED
+            if self.y() >= 746:
+                dy -= 0
+            else:
+                dy += PLAYER_SPEED
             self.setimage2("Image/tank2Bottom")
+        playerPositions[1] = (self.x() + dx, self.y() + dy)
+        #print(self.playerPositions[1])
         self.setPos(self.x()+dx, self.y()+dy)
 
     def setimage(self, image):
@@ -110,7 +172,9 @@ class Player(QGraphicsPixmapItem):
 class Bullet(QGraphicsPixmapItem):
     def __init__(self, offset_x, offset_y, parent = None):
         QGraphicsPixmapItem.__init__(self,parent)
-        self.setPixmap(QPixmap("Image/bulet"))
+        self.imageBullet = "Image/bulet"
+        self.imageBullet2 = "Image/bulet"
+        self.setPixmap(QPixmap(self.imageBullet))
         self.offset_x = offset_x
         self.offset_y = offset_y
         self.active = False
@@ -120,22 +184,34 @@ class Bullet(QGraphicsPixmapItem):
         self.bulletDirection = 0
         self.bulletDirection2 = 0
 
+    def setBulletImage(self, image):
+        self.imageBullet = image
+        self.setPixmap(QPixmap(self.imageBullet))
+
+    def setBulletImage2(self, image):
+        self.imageBullet2 = image
+        self.setPixmap(QPixmap(self.imageBullet2))
+
     def game_update(self, keys_pressed, player):
         if not self.active:
             if Qt.Key_Space in keys_pressed:
                 #print(player.image)
                 if player.getimage() == "Image/tankTop":
+                    self.setBulletImage("Image/bulet")
                     self.bulletDirection = 0
                     self.setPos(player.x() + self.offset_x, player.y() + self.offset_y)
                 elif player.getimage() == "Image/tankRight":
+                    self.setBulletImage("Image/buletHorizontal")
                     self.bulletDirection = 1
-                    self.setPos(player.x() + self.offset_x, player.y() + self.offset_y)
+                    self.setPos(player.x() + self.offset_x, player.y() + self.offset_y+8)
                 elif player.getimage() == "Image/tankBottom":
+                    self.setBulletImage("Image/bulet")
                     self.bulletDirection = 2
                     self.setPos(player.x() + self.offset_x, player.y() + self.offset_y)
                 elif player.getimage() == "Image/tankLeft":
+                    self.setBulletImage("Image/buletHorizontal")
                     self.bulletDirection = 3
-                    self.setPos(player.x() + self.offset_x, player.y() + self.offset_y)
+                    self.setPos(player.x() + self.offset_x, player.y() + self.offset_y+8)
 
                 self.active = True
                 self.frames = BULLET_FRAMES
@@ -146,6 +222,10 @@ class Bullet(QGraphicsPixmapItem):
                 self.setPos(self.x(),self.y()-BULLET_SPEED)
             elif self.bulletDirection == 2:
                 self.setPos(self.x(), self.y() + BULLET_SPEED)
+            elif self.bulletDirection == 1:
+                self.setPos(self.x() + BULLET_SPEED, self.y())
+            elif self.bulletDirection == 3:
+                self.setPos(self.x() - BULLET_SPEED, self.y())
             self.frames -= 1
             if self.frames <= 0:
                 self.active = False
@@ -156,17 +236,21 @@ class Bullet(QGraphicsPixmapItem):
             if Qt.Key_Control in keys_pressed:
                 print(player.getimage2())
                 if player.getimage2() == "Image/tank2Top":
+                    self.setBulletImage2("Image/bulet")
                     self.bulletDirection2 = 0
                     self.setPos(player.x() + self.offset_x, player.y() + self.offset_y)
                 elif player.getimage2() == "Image/tank2Right":
+                    self.setBulletImage2("Image/buletHorizontal")
                     self.bulletDirection2 = 1
-                    self.setPos(player.x() + self.offset_x, player.y() + self.offset_y)
+                    self.setPos(player.x() + self.offset_x, player.y() + self.offset_y+9)
                 elif player.getimage2() == "Image/tank2Bottom":
+                    self.setBulletImage2("Image/bulet")
                     self.bulletDirection2 = 2
                     self.setPos(player.x() + self.offset_x, player.y() + self.offset_y)
                 elif player.getimage2() == "Image/tank2Left":
+                    self.setBulletImage2("Image/buletHorizontal")
                     self.bulletDirection2 = 3
-                    self.setPos(player.x() + self.offset_x, player.y() + self.offset_y)
+                    self.setPos(player.x() + self.offset_x, player.y() + self.offset_y+9)
 
                 self.active2 = True
                 self.frames2 = BULLET_FRAMES
@@ -176,6 +260,10 @@ class Bullet(QGraphicsPixmapItem):
                 self.setPos(self.x(),self.y()-BULLET_SPEED)
             elif self.bulletDirection2 == 2:
                 self.setPos(self.x(), self.y() + BULLET_SPEED)
+            elif self.bulletDirection2 == 1:
+                self.setPos(self.x() + BULLET_SPEED, self.y())
+            elif self.bulletDirection2 == 3:
+                self.setPos(self.x() - BULLET_SPEED, self.y())
             self.frames2 -= 1
             if self.frames2 <= 0:
                 self.active2 = False
@@ -193,23 +281,29 @@ class Scene(QGraphicsScene):
         self.timer.start(FRAME_TIME_MS, self)
 
         bg = QGraphicsRectItem()
-        bg.setRect(-1,-1,SCREEN_WIDTH+2,SCREEN_HEIGHT+2)
+        bg.setRect(-1,-1,1010+3,SCREEN_HEIGHT+2)
         bg.setBrush(QBrush(Qt.black))
         self.addItem(bg)
 
         self.player1 = Player("Image/tankTop")
-        self.player1.setPos((SCREEN_WIDTH-self.player1.pixmap().width())/4,
-                           (SCREEN_HEIGHT-self.player1.pixmap().height())/4)
+        self.player1.setPos((SCREEN_WIDTH-self.player1.pixmap().width())/5,
+                           (SCREEN_HEIGHT-self.player1.pixmap().height())/1)
         self.player2 = Player("Image/tank2Top")
-        self.player2.setPos((SCREEN_WIDTH - self.player2.pixmap().width()) / 2,
-                            (SCREEN_HEIGHT - self.player2.pixmap().height()) / 2)
+        self.player2.setPos((SCREEN_WIDTH - self.player2.pixmap().width()) / 3*2,
+                            (SCREEN_HEIGHT - self.player2.pixmap().height()) / 1)
 
-        self.bullets = [Bullet(PLAYER_BULLET_X_OFFSETS[0],PLAYER_BULLET_Y)]
+        self.bullets = [Bullet(PLAYER_BULLET_X_OFFSETS,PLAYER_BULLET_Y)]
         for b in self.bullets:
             b.setPos(SCREEN_WIDTH,SCREEN_HEIGHT)
             self.addItem(b)
+        self.bullets2 = [Bullet(PLAYER_BULLET_X_OFFSETS, PLAYER_BULLET_Y)]
+        for b2 in self.bullets2:
+            b2.setPos(SCREEN_WIDTH, SCREEN_HEIGHT)
+            self.addItem(b2)
         self.addItem(self.player1)
         self.addItem(self.player2)
+
+
 
         self.view = QGraphicsView(self)
         self.view.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
@@ -217,6 +311,7 @@ class Scene(QGraphicsScene):
         self.view.show()
         self.view.setFixedSize(SCREEN_WIDTH,SCREEN_HEIGHT)
         self.setSceneRect(0,0,SCREEN_WIDTH,SCREEN_HEIGHT)
+        self.view.setWindowTitle("Battle City")
 
     def keyPressEvent(self, event):
         self.keys_pressed.add(event.key())
@@ -233,8 +328,8 @@ class Scene(QGraphicsScene):
         for b in self.bullets:
             b.game_update(self.keys_pressed, self.player1)
         self.player2.game_update2(self.keys_pressed)
-        for b in self.bullets:
-            b.game_update2(self.keys_pressed, self.player2)
+        for b2 in self.bullets2:
+            b2.game_update2(self.keys_pressed, self.player2)
 
 
 if __name__ == '__main__':
